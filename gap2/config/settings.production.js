c6110e483b9c4927901037af79d5932ed4e160da
enridaga <enricodaga@gmail.com>
2014-01-11 22:16:11 +0000

    Deployed latest changes (about #19)

--- gap2/config/settings.production.js
+++ gap2/config/settings.production.js
@@ -4,3 +4,4 @@ API_ROOT = 'http://gap2.alexandriaarchive.org';
 REPORT_URL = 'http://gap2.alexandriaarchive.org/flags/';
 API_DATA_TYPE = 'json';
 DEBUG = false;
+REPORT_BAD_TOKEN_URL = "http://gap2.alexandriaarchive.org/report/token-issues/{token-id}";
