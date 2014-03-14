e40bd10614c386561f72eb6cf0d1c62f902329a5
enridaga <enricodaga@gmail.com>
2014-03-14 13:56:06 +0000

    Deploying test instances for classic (/gap) and hestia (/gap2) versions of GapVis.

--- gap2/config/settings.production.js
+++ gap2/config/settings.production.js
@@ -4,4 +4,18 @@ REPORT_URL = 'http://gap2.alexandriaarchive.org/flags/';
 API_DATA_TYPE = 'json';
 DEBUG = false;
 REPORT_BAD_TOKEN_URL = "http://gap2.alexandriaarchive.org/report/token-issues/{token-id}";
-VIEW_ON = "View on Perseus";
+VIEW_ON = "Perseus";
+VIEW_ON_LINK = function(uri, page){
+	// uri is the link to the book page on the origin repository
+	// page is the page number (eg, 1, 3, -13) or reference (eg. 1.1, 4.5)
+	// Implement here how to build a more detailed link
+	
+	// XXX This is an example for Perseus repository
+	// uri is something like http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.01.0126
+	if(page.indexOf('.')!=-1){
+		page = page.split('.');
+		return uri + '%3Abook%3D' + page[0] + '%3Achapter%3D' + page[1] + '%3A';		
+	}else{
+		return uri;
+	}
+}
