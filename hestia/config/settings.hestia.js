77cfe872b0d421e9e2535ec27203eead584b8446
enridaga <enricodaga@gmail.com>
2017-03-13 22:56:36 +0000

    disabled feedback link

--- hestia/config/settings.hestia.js
+++ hestia/config/settings.hestia.js
@@ -3,7 +3,7 @@ API_ROOT = 'tests/api';
 REPORT_URL = 'tests/api/flags/';
 // API_DATA_TYPE = 'json';
 // DEBUG = true;
-// disableChangeLink = true;
+disableChangeLink = true;
 // //if present and not null, makes a direct link isntead of a form. Looks for {token-id}
 // REPORT_BAD_TOKEN_URL = null;
 // //if present and not null, makes a direct link isntead of a form. Looks for {place-id}
